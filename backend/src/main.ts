90a53006f863dda49257c11f33d7d52ba4d08452
Federico Arambarri <v-fearam@microsoft.com>
2026-02-14 17:48:29 -0300

    backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,6 +6,25 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-  await app.listen(process.env.PORT ?? 3000);
+
+  if (process.env.VERCEL) {
+    await app.init();
+    return app.getHttpAdapter().getInstance();
+  } else {
+    await app.listen(process.env.PORT ?? 3000);
+  }
 }
-bootstrap();
+
+let handler: any;
+
+export default async (req: any, res: any) => {
+  if (!handler) {
+    handler = await bootstrap();
+  }
+  return handler(req, res);
+};
+
+if (!process.env.VERCEL) {
+  bootstrap();
+}
+
